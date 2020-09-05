a6278741b7285a82b3767c3500e10954b87e428c
AKSHAY <70620976+codexakshay@users.noreply.github.com>
2020-09-05 13:31:13 +0530

    Update employee_wage.sh

--- employee_wage.sh
+++ employee_wage.sh
@@ -1,17 +1,43 @@
-isPartTime=1;
-isFullTime=2;
-empRatePerHr=20;
-randomCheck=$(($RANDOM%3));
-case $randomCheck in
-        $isFullTime)
-                empHrs=8;
-                ;;
-        $isPartTime)
-                empHrs=4;
-                ;;
-        *)
-                empHrs=0;
-                ;;
+#!/bin/bash -x
+
+# CONSTATNTS FOR THE PROGRAM
+IS_PART_TIME=1;
+IS_FULL_TIME=2;
+MAX_HRS_IN_MONTH=100;
+EMP_RATE_PER_HR=20;
+NUM_WORKING_DAYS=20;
+
+# VARIABLES
+totalEmpHrs=0;
+totalWorkingDays=0;
+empHrs=0
+function getWorkHrs(){
+local empCheck=$1
+case $empCheck in
+  $IS_FULL_TIME)
+      empHrs=8
+         ;;
+  $IS_PART_TIME)
+       empHrs=4
+   ;;
+  *)
+    empHrs=0
+        ;;
 esac
-salary=$(($empHrs*$empRatePerHr));
-echo "EMPLOYEE SALARY IS : $salary";
+echo $empHrs
+}
+
+
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+do
+totalEmpHrs=$(($totalEmpHrs+$empHrs))
+((totalWorkingDays++))
+empCheck=$(($RANDOM%3));
+empHrs=$( getWorkHrs $empCheck )
+totalEmpHrs=$(( $totalEmpHrs + $empHrs ));
+dailyWage[$totalWorkingDays]=$(( $empHrs*$EMP_RATE_PER_HR ))
+done
+totalSalary=$(($totalEmpHrs*$EMP_RATE_PER_HR))
+echo "DAILY WAGE :" ${dailyWage[@]}
+echo "TOTAL DAYS : "$totalWorkingDays
+echo "TOTAL WAGE : "$totalSalary
