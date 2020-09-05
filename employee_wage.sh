382d8066e8458328aeba9b11dc4d9e4fb91c6579
AKSHAY <70620976+codexakshay@users.noreply.github.com>
2020-09-05 13:17:16 +0530

    Update employee_wage.sh

--- employee_wage.sh
+++ employee_wage.sh
@@ -1,17 +1,31 @@
-isPartTime=1;
-isFullTime=2;
-empRatePerHr=20;
-randomCheck=$(($RANDOM%3));
-case $randomCheck in
-        $isFullTime)
-                empHrs=8;
-                ;;
-        $isPartTime)
-                empHrs=4;
-                ;;
-        *)
-                empHrs=0;
-                ;;
+#!/bin/bash -x
+
+#CONSTANTS
+IS_PART_TIME=1;
+IS_FULL_TIME=2;
+MAX_HRS_IN_MONTH=10;
+EMP_RATE_PER_HR=20;
+NUM_WORKING_DAYS=20;
+
+# VARIABLES
+totalEmpHrs=0;
+totalWorkingDays=0;
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+do
+((totalWorkingDays++))
+empCheck=$(($RANDOM%3));
+case $empCheck in
+  $IS_FULL_TIME)
+      empHrs=8
+         ;;
+  $IS_PART_TIME)
+       empHrs=4
+        ;;
+  *)
+    empHrs=0
+   ;;
 esac
-salary=$(($empHrs*$empRatePerHr));
-echo "EMPLOYEE SALARY IS : $salary";
+totalEmpHrs=$(( $totalEmpHrs + $empHrs ));
+done
+
+totalSalary=$(($totalEmpHrs*$EMP_RATE_PER_HR))
